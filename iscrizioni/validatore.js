6fa7eb6d4caea4389490b111fd81f18de7b50685
gCap-pixel <gcappellin9@gmail.com>
2025-10-06 12:43:07 +0200

    implementazione del js, verifica della stringa in modo tale che sia composta solamente da caratteri, aggiunta della convalidazione della email sul js

---
+++ iscrizioni/validatore.js
@@ -0,0 +1,40 @@
+function onlylettere(string){
+    var lettere= /^[a-zA-Z]+$/;
+    if (string.match(lettere)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function validazioneEmail(string){
+    var lettere=/[a-z0-9A-Z]*@[a-zA-Z]*[.][a-zA-Z]*/
+    if (string.match(lettere)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function email(stringa){
+    if (stringa.length>0 && validazioneEmail(stringa)){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+function validate_input(form){
+	var error_message="";
+	if(validazioneEmail(form.email.value)){
+		error_message+="email corretta";
+	}
+	if(error_message.length>0){
+		alert(error_message);
+		return false;
+	}
+	else{
+		return true;
+	}
+}
