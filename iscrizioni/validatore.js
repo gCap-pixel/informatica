9e973c994907dd2eadd3d091035ceffbdcadab80
gCap-pixel <gcappellin9@gmail.com>
2025-10-07 13:19:32 +0200

    aggiunta della funzione "validazioneCodice" e della funzione "validazioneIva", entrambe controllano se le informazioni inserite nella pagina HTML rispettano il pattern da me inserito, se ci sono errori li stampo a schermo con un alert, ho riscontrato problemi sul convalidatore in js del codice fiscale, l'errore stava in una distrazione dove ho messo "stringa === 16" invece di "stringa.length===16"

--- iscrizioni/validatore.js
+++ iscrizioni/validatore.js
@@ -1,21 +1,58 @@
-function onlylettere(string){
-    var lettere= /^[a-zA-Z]+$/;
-    if (string.match(lettere)){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 function validazioneEmail(string){
-    var lettere=/[a-z0-9A-Z]*@[a-zA-Z]*[.][a-zA-Z]*/
-    if (string.match(lettere)){
+    var validatore=/[a-z0-9A-Z]*@[a-zA-Z]*[.][a-zA-Z]*/
+    if (string.match(validatore)){
         return true
     }
     else{
         return false
     }
 }
+function validazioneIva(string){
+    var validatore=/[0-9]*/
+    if (string.match(validatore)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function validazioneCodice(string){
+
+    var validatore=/^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/  /*CPPGRL07S02D653Z*/
+
+    if(string.match(validatore)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+
+
+
+function iva(stringa){
+    if ((stringa.length>0) && (validazioneIva(stringa)) && (stringa.length===11)){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+function codice(stringa){
+
+        if (validazioneCodice(stringa) && stringa.length === 16){
+
+        return true
+
+    }
+    else{
+        alert("OK");
+        return false
+    }
+}
+
 function email(stringa){
     if (stringa.length>0 && validazioneEmail(stringa)){
         return true;
@@ -25,12 +62,19 @@ function email(stringa){
     }
 }
 
+
+
 function validate_input(form){
 	var error_message="";
-	if(!email(form.email.value)){
-		error_message+="email sbagliata";
+	if(!email(form.mail.value)){
+		error_message+="email non valida \n"
 	}
-
+    if(!iva(form.partitaIva.value)){
+        error_message+="partita iva non valida \n"
+    }
+    if(!codice(form.codiceF.value)){
+        error_message+="codice fiscale non valido \n"
+    }
 	if(error_message.length>0){
 		alert(error_message);
 		return false;
